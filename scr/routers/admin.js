c3bbc703c683fad21606bbf7c1d49f8f043a7aed
eyad kadry <eyadmoed08@gmail.com>
2024-08-16 19:00:27 +0300

    add get profile and get all teacher request to admin

--- scr/routers/admin.js
+++ scr/routers/admin.js
@@ -9,5 +9,6 @@ router.post("/login",apiHandler(controller.login));
 router.put("/",apiHandler(verifyAdminToken),apiHandler(controller.update));
 router.post("/rejectTeacher/:teacherId",apiHandler(verifyAdminToken),apiHandler(controller.rejectTeacher));
 router.get("/acceptTeacher/:teacherId",apiHandler(verifyAdminToken),apiHandler(controller.acceptTeacher));
-
+router.get("/getAllTeacherRequest",apiHandler(verifyAdminToken),apiHandler(controller.getAllTeacher))
+router.get("/getProfile",apiHandler(verifyAdminToken),apiHandler(controller.getProfile))
 module.exports = router;
